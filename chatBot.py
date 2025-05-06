c94e1c7efe0dd289899853cafeebd351fbefd729
disouzadev231 <disouzadevfull@gmail.com>
2025-05-05 23:46:00 -0300

    novo agente no dialogflow

--- chatBot.py
+++ chatBot.py
@@ -34,7 +34,7 @@ def detect_intent_text(
     msg,
     session_id="sessao_123",
     project_id="careful-alloy-433019",
-    agent_id="3e7c7703-9ad7-4943-ab42-954363eda079",
+    agent_id="8814b38a-995d-4bab-8290-5e51472f5650",
     location="us-central1",
     language_code="pt-BR"
 ):
